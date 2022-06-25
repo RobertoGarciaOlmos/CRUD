d76692981682426e4578359344582eb5a8cfb078
Robert <roberto.garcia.olmos@gmail.com>
2022-06-25 00:59:00 -0500

    Ante proyecto

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-console.log("hola!!!")
+const input = document.getElementById("input")
+
+function imprimirValorInput() {
+    console.log(input.value);
+}
+
+const boton = document.getElementsByClassName("boton");
+
+const lable = document.querySelector("label");
+
+// Crear un elemento (metodo1)
+
+
+
+var Imagen = document.createElement("img");
+
+let perro = document.querySelector("body");
+perro.appendChild(Imagen);
+
+
+
+Imagen.src = "https://tse3.mm.bing.net/th?id=OIP.6VxupNs7ee2hyKlMiGyAcQHaGL&pid=Api&P=0&w=199&h=166"
+
+
+var Imagen2 = document.createElement("img");
+
+
+let perro2 = document.querySelector("header");
+perro.appendChild(Imagen2);
+
+
+
+Imagen2.src = "https://tse3.mm.bing.net/th?id=OIP.6VxupNs7ee2hyKlMiGyAcQHaGL&pid=Api&P=0&w=199&h=166"
+
+var imgperro = "https://tse3.mm.bing.net/th?id=OIP.6VxupNs7ee2hyKlMiGyAcQHaGL&pid=Api&P=0&w=199&h=166"
+
+
+
+let miboton = document.getElementById("boton1");
+
+function miFuncion(evento) {
+    console.log("hiciste click!")
+    perro2.innerHTML += "ya casi"
+
+
+}
+
+miboton.addEventListener("click", miFuncion)
