795ceb798665a51e6b0c98c7fff8c17fcf7f19c8
Robert <roberto.garcia.olmos@gmail.com>
2022-06-25 02:01:54 -0500

    FORMATO TERMINADO

--- main.js
+++ main.js
@@ -1,48 +1,48 @@
-const input = document.getElementById("input")
+//const input = document.getElementById("input")
 
-function imprimirValorInput() {
-    console.log(input.value);
-}
+// function imprimirValorInput() {
+// console.log(input.value);
+// }
 
-const boton = document.getElementsByClassName("boton");
+// const boton = document.getElementsByClassName("boton");
 
-const lable = document.querySelector("label");
+// const lable = document.querySelector("label");
 
 // Crear un elemento (metodo1)
 
 
 
-var Imagen = document.createElement("img");
+// var Imagen = document.createElement("img");
 
-let perro = document.querySelector("body");
-perro.appendChild(Imagen);
-
-
-
-Imagen.src = "https://tse3.mm.bing.net/th?id=OIP.6VxupNs7ee2hyKlMiGyAcQHaGL&pid=Api&P=0&w=199&h=166"
-
-
-var Imagen2 = document.createElement("img");
-
-
-let perro2 = document.querySelector("header");
-perro.appendChild(Imagen2);
+// let perro = document.querySelector("body");
+// perro.appendChild(Imagen);
 
 
 
-Imagen2.src = "https://tse3.mm.bing.net/th?id=OIP.6VxupNs7ee2hyKlMiGyAcQHaGL&pid=Api&P=0&w=199&h=166"
-
-var imgperro = "https://tse3.mm.bing.net/th?id=OIP.6VxupNs7ee2hyKlMiGyAcQHaGL&pid=Api&P=0&w=199&h=166"
+// Imagen.src = "https://tse3.mm.bing.net/th?id=OIP.6VxupNs7ee2hyKlMiGyAcQHaGL&pid=Api&P=0&w=199&h=166"
 
 
-
-let miboton = document.getElementById("boton1");
-
-function miFuncion(evento) {
-    console.log("hiciste click!")
-    perro2.innerHTML += "ya casi"
+// var Imagen2 = document.createElement("img");
 
 
-}
+// let perro2 = document.querySelector("header");
+// perro.appendChild(Imagen2);
 
-miboton.addEventListener("click", miFuncion)
+
+
+// Imagen2.src = "https://tse3.mm.bing.net/th?id=OIP.6VxupNs7ee2hyKlMiGyAcQHaGL&pid=Api&P=0&w=199&h=166"
+
+// var imgperro = "https://tse3.mm.bing.net/th?id=OIP.6VxupNs7ee2hyKlMiGyAcQHaGL&pid=Api&P=0&w=199&h=166"
+
+
+
+// let miboton = document.getElementById("boton1");
+
+// function miFuncion(evento) {
+// console.log("hiciste click!")
+// perro2.innerHTML += "ya casi"
+
+
+// }
+
+// miboton.addEventListener("click", miFuncion)
